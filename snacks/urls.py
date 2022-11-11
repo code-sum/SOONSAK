be7f736ff214500abfb93e77a9703452ea23ca0a
leesc8408 <leesc8408@gmail.com>
2022-11-11 16:36:19 +0900

    스낵/디테일페이지_시작

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'snacks'
 
 urlpatterns = [
+  path("detail/<int:snack_pk>", views.detail, name="detail"),
+  path("delete/<int:snack_pk>", views.delete, name="delete"),
 
 ]
