676f61ad1d8bef1978626b02b727d8058180d6ad
leesc8408 <leesc8408@gmail.com>
2022-11-14 09:30:32 +0900

    디테일 프론트 작업중

--- snacks/admin.py
+++ snacks/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Snacks,snack_Category
 
 # Register your models here.
+# admin에서 모델이 보이도록
 admin.site.register(Snacks)
 admin.site.register(snack_Category)
