ef6e1224a77271f9d88c2f5837671347b1356964
han-gaeul <gmlakdcjs314@gmail.com>
2022-11-16 00:07:37 +0900

    order admin 수정 / 관리자 주문내역 관리 페이지 생성 및 배송 상태변경 기능 추가

--- orders/urls.py
+++ orders/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('delete/<int:order_pk>/', views.delete, name="delete"),
     # 주문 변경
     path('update/<int:order_pk>/', views.update, name="update"),
+    # 관리자 주문 내역
+    path('order_list/', views.order_list, name='order_list'),
+    # 배송 준비중
+    path('delivery/<int:order_pk>/', views.delivery, name='delivery'),
+    # 배송 완료
+    path('delivery_complete/<int:order_pk>/', views.delivery_complete, name='delivery_complete')
 ]
