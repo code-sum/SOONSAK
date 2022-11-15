2f7d7c4ee49c56a0c8a0d7506663fb4a91f3ee1c
code-sum <yisugyeong@gmail.com>
2022-11-15 11:30:34 +0900

    accounts/signup.html 수정완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,8 +15,12 @@ class CustomUserCreationForm(UserCreationForm):
             'profile_image',
             'address',
         ]
-
-        labels = {"profile_image":"프로필 이미지", "address": "주소"}
+        labels = {"username": "아이디", "profile_image":"프로필 이미지", "address": "주소"}
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for fieldname in ['password1']:
+            self.fields[fieldname].help_text = None
+print(CustomUserCreationForm())
 
 # 회원 프로필수정 폼
 class CustomUserChangeForm(UserChangeForm):
